c398da552d752dd1c0fdbd79433786626b12c263
Lucas Dionisi <lucas.dionisi@gmail.com>
2021-10-13 11:32:09 +0200

    Comment JavaFx code

--- Timeline/src/timeline/Timeline.java
+++ Timeline/src/timeline/Timeline.java
@@ -21,7 +21,7 @@ public class Timeline extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
+        /*Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -38,14 +38,14 @@ public class Timeline extends Application {
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show();*/
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
     }
     
 }
